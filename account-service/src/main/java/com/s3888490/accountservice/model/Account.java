47005d20958e6548d86a7e27c42e3b9f2a5e8a2d
Luke <lukemacdonald21@gmail.com>
2022-10-11 11:42:35 +1100

    created model for account and account type

---
+++ account-service/src/main/java/com/s3888490/accountservice/model/Account.java
@@ -0,0 +1,87 @@
+package com.s3888490.accountservice.model;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+public class Account {
+
+    @Id
+    @Column(name = "account_number")
+    private String accountNumber;
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "account_type", columnDefinition = "ENUM('TermInvestment', 'Loan', 'Saving')")
+    @Enumerated(EnumType.STRING)
+    private AccountType accountType;
+
+    @Column(name = "account_name")
+    private String accountName;
+
+    @Column(name = "balance")
+    private String balance;
+
+    @Column(name = "date")
+    private Date date = new Date(System.currentTimeMillis());;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getAccountType() {
+        return accountType.getAccountTypeName();
+    }
+
+    public void setAccountType(String accountType) {
+        String upper = accountType.toUpperCase().trim();
+        switch (upper){
+            case("TERMINVESTMENT"):
+            case("TERM INVESTMENT"):
+                this.accountType = AccountType.TermInvestment;
+                break;
+            case("LOAN"):
+                this.accountType = AccountType.Loan;
+                break;
+            case("SAVING"):
+                this.accountType = AccountType.Saving;
+                break;
+            default:
+                this.accountType= null;
+        }
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date=date;
+    }
+}
---
+++ account-service/src/main/java/com/s3888490/accountservice/model/AccountType.java
@@ -0,0 +1,20 @@
+package com.s3888490.accountservice.model;
+
+public enum AccountType {
+    TermInvestment(1),
+    Loan(2),
+    Saving(3);
+
+    private final int value;
+
+    AccountType(final int value) {
+        this.value = value;
+    }
+
+    public int getAccountTypeValue() {
+        return this.value;
+    }
+    public String getAccountTypeName() {
+        return this.name();
+    }
+}
