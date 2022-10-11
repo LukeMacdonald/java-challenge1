de98e50a3c3b67e8fbd075895960b7c0330306d7
Luke <lukemacdonald21@gmail.com>
2022-10-11 23:15:26 +1100

    final

--- FundManagement/person-service/src/test/java/com/s3888490/personservice/PersonServiceApplicationTests.java
+++ FundManagement/person-service/src/test/java/com/s3888490/personservice/PersonServiceApplicationTests.java
@@ -1,25 +1,12 @@
 package com.s3888490.personservice;
 
-
 import com.s3888490.personservice.controller.PersonController;
-
 import org.assertj.core.api.Assertions;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
