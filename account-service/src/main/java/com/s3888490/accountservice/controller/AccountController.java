efa592480f0f8e9271def757b5f55d55254c8ee6
Luke <lukemacdonald21@gmail.com>
2022-10-11 12:27:50 +1100

    created post call for account microservice

--- account-service/src/main/java/com/s3888490/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/s3888490/accountservice/controller/AccountController.java
@@ -1,10 +1,31 @@
 package com.s3888490.accountservice.controller;
 
+import com.s3888490.accountservice.model.Account;
+import com.s3888490.accountservice.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/accounts")
+@RequiredArgsConstructor
 public class AccountController {
+
+
+    private final AccountService accountService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> savePerson(@RequestBody Account account){
+        Account newAccount = accountService.saveAccount(account);
+        if(newAccount != null){
+            return ResponseEntity.ok().body(newAccount);
+        }
+        else{
+            return ResponseEntity.badRequest().body("Account with same number already exists");
+        }
+    }
 
 }
--- account-service/src/main/java/com/s3888490/accountservice/service/AccountService.java
+++ account-service/src/main/java/com/s3888490/accountservice/service/AccountService.java
@@ -1,7 +1,20 @@
 package com.s3888490.accountservice.service;
 
+import com.s3888490.accountservice.dao.AccountRepository;
+import com.s3888490.accountservice.model.Account;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public Account saveAccount(Account account) {
+        if(accountRepository.findAccountByAccountNumber(account.getAccountNumber())==null){
+            return  accountRepository.save(account);
+        }
+        return null;
+    }
 }
